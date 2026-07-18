023615b3ebf312a6a358f1b43b48724fb6b68136
MD ABDUL AWAL <awal3web@gmail.com>
2026-07-19 00:12:13 +0600

    Update Index

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db.js';
-import appointmentRoutes from './routes/appointmentRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import appointmentRoutes from "./routes/appointmentRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -10,15 +10,22 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
-app.use('/api/appointments', appointmentRoutes);
+app.use("/api/appointments", appointmentRoutes);
 
 // Root Route
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 5000;
