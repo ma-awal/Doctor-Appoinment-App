1793df8d86912c6424f9321346d30fe9f4a5edf9
MD ABDUL AWAL <awal3web@gmail.com>
2026-07-19 00:43:23 +0600

    Update cors

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,14 +8,17 @@ export const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [appointments, setAppointments] = useState([]);
 
+  const API_URL =
+    "https://doctor-appointment-app-4d5o.onrender.com/api/appointments";
+
   const bookAppointment = async (formData) => {
     setLoading(true);
     try {
-      const response = await axios.post(
-        "https://doctor-appointment-app-4d5o.onrender.com/api/appointments",
-        formData
-      );
+      const response = await axios.post(API_URL, formData);
       toast.success(response.data.message);
+
+      await fetchAppointments();
+
       return true;
     } catch (error) {
       toast.error(error.response?.data?.message || "Booking Failed");
@@ -28,9 +31,7 @@ export const AppProvider = ({ children }) => {
   const fetchAppointments = async () => {
     setLoading(true);
     try {
-      const response = await axios.get(
-        "http://localhost:5000/api/appointments"
-      );
+      const response = await axios.get(API_URL);
       setAppointments(response.data.data);
     } catch (error) {
       console.error("Error fetching data", error);
