a36e468efb1a17f23324e1c02cdd2dc6cf3a872d
MD ABDUL AWAL <awal3web@gmail.com>
2026-07-19 00:35:52 +0600

    Fix Final

--- server/index.js
+++ server/index.js
@@ -11,16 +11,16 @@ const app = express();
 
 app.use(cors());
 
-app.options("*", cors());
-
 app.use(express.json());
 
 app.get("/api/test", (req, res) => {
   res.json({ message: "API is working perfectly!" });
 });
 
+// ৩. আপনার মেইন রুট
 app.use("/api/appointments", appointmentRoutes);
 
+// ৪. হোম রুট
 app.get("/", (req, res) => {
   res.send("CarePulse Server is Live...");
 });
